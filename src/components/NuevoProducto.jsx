1eb3ef7d0c3747bd58d68fa37a143e99c4f9a464
Sirio2022 <105917953+Sirio2022@users.noreply.github.com>
2023-05-08 17:44:07 -0500

    first commit

---
+++ src/components/NuevoProducto.jsx
@@ -0,0 +1,45 @@
+export default function NuevoProducto() {
+  return (
+    <div className="row justify-content-center">
+      <div className="col-md-8">
+        <div className="card">
+          <div className="card-body">
+            <h2 className="text-center mb-4 font-weight-bold">
+              Agregar Nuevo Producto
+            </h2>
+
+            <form>
+              <div className="form-group">
+                <label htmlFor="nombre">Nombre del producto</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Nombre del producto"
+                  id="nombre"
+                  name="nombre"
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="precio">Precio del producto</label>
+                <input
+                  type="number"
+                  className="form-control"
+                  placeholder="Precio del producto"
+                  id="precio"
+                  name="precio"
+                />
+              </div>
+
+              <button
+                type="submit"
+                className="btn btn-primary font-weight-bold text-uppercase d-block w-100"
+              >
+                Agregar
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
