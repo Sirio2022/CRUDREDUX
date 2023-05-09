78dfc37d6e476738f83cec48a394977d78a64f3e
Sirio2022 <105917953+Sirio2022@users.noreply.github.com>
2023-05-09 11:17:34 -0500

    Muy bueno

--- src/actions/productoActions.jsx
+++ src/actions/productoActions.jsx
@@ -3,34 +3,39 @@ import {
   AGREGAR_PRODUCTO_EXITO,
   AGREGAR_PRODUCTO_ERROR,
 } from '../types';
+import clienteAxios from '../config/axios';
 
 // Crear nuevos productos
 export function crearNuevoProductoAction(producto) {
-  return (dispatch) => {
+  return async (dispatch) => {
     dispatch(agregarProducto());
 
     try {
-        dispatch(agregarProductoExito(producto));
+      // Insertar en la API
+      await clienteAxios.post('/productos', producto);
+      // Si todo sale bien, actualizar el state
+      dispatch(agregarProductoExito(producto));
     } catch (error) {
-        dispatch(agregarProductoError(true));
+      console.log(error);
+      // Si hay un error, cambiar el state
+      dispatch(agregarProductoError(true));
     }
   };
 }
 
 const agregarProducto = () => ({
-    type: AGREGAR_PRODUCTO,
-    payload: true,
-  
+  type: AGREGAR_PRODUCTO,
+  payload: true,
 });
 
 // Si el producto se guarda en la base de datos
 const agregarProductoExito = (producto) => ({
-    type: AGREGAR_PRODUCTO_EXITO,
-    payload: producto,
-})
+  type: AGREGAR_PRODUCTO_EXITO,
+  payload: producto,
+});
 
 // Si hubo un error
 const agregarProductoError = (estado) => ({
-    type: AGREGAR_PRODUCTO_ERROR,
-    payload: estado
+  type: AGREGAR_PRODUCTO_ERROR,
+  payload: estado,
 });
