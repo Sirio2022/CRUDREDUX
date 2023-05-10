7573a88a20b3b29cd447b348aaa18bb3d9c8d7c8
Sirio2022 <105917953+Sirio2022@users.noreply.github.com>
2023-05-10 10:18:55 -0500

    Nice!

--- src/components/NuevoProducto.jsx
+++ src/components/NuevoProducto.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom';
 
 // Actions de Redux
 import { crearNuevoProductoAction } from '../actions/productoActions';
+import { mostrarAlerta, ocultarAlertaAction } from '../actions/alertaActions';
 
 export default function NuevoProducto() {
   // State del componente
@@ -18,7 +19,8 @@ export default function NuevoProducto() {
 
   // Acceder al state del store
   const cargando = useSelector((state) => state.productos.loading);
-  const error = useSelector((state) => state.productos.error)
+  const error = useSelector((state) => state.productos.error);
+  const alerta = useSelector((state) => state.alerta.alerta)
 
   // Mandar llamar el action de productoAction
   const agregarProducto = (producto) =>
@@ -28,10 +30,16 @@ export default function NuevoProducto() {
     e.preventDefault();
     // Validar formulario
     if (nombre.trim() === '' || precio <= 0) {
+      const alerta = {
+        msg: 'Ambos campos son obligatorios',
+        classes: 'alert alert-danger text-center thext-uppercase p3',
+      };
+      dispatch(mostrarAlerta(alerta));
       return;
     }
 
     // Si no hay errores
+    dispatch(ocultarAlertaAction());
 
     // Crear el nuevo producto
     agregarProducto({
@@ -50,6 +58,7 @@ export default function NuevoProducto() {
             <h2 className="text-center mb-4 font-weight-bold">
               Agregar Nuevo Producto
             </h2>
+            {alerta ? <p className={alerta.classes}>{alerta.msg}</p> : null}
 
             <form onSubmit={submitNuevoProducto}>
               <div className="form-group">
@@ -85,7 +94,11 @@ export default function NuevoProducto() {
               </button>
             </form>
             {cargando ? <p>Cargando...</p> : null}
-            {error ? <p className='alert alert-danger p2 mt-4 text-center'>Hubo un error</p> : null}
+            {error ? (
+              <p className="alert alert-danger p2 mt-4 text-center">
+                Hubo un error
+              </p>
+            ) : null}
           </div>
         </div>
       </div>
